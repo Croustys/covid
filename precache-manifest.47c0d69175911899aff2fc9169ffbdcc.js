b2ae6c8e1ca60d22e8fda9c56e23e78ff5553837
Croustys <51267148+Croustys@users.noreply.github.com>
2020-04-13 19:54:47 +0200

    Updates

---
+++ precache-manifest.47c0d69175911899aff2fc9169ffbdcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e383a2ae48a72e5a7c326373fd28bb",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "9ca15df29a37a435290c",
+    "url": "/covid/static/css/main.c8bae3a1.chunk.css"
+  },
+  {
+    "revision": "acc7f465e2dc87fe11b8",
+    "url": "/covid/static/js/2.b769a716.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covid/static/js/2.b769a716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca15df29a37a435290c",
+    "url": "/covid/static/js/main.589facc0.chunk.js"
+  },
+  {
+    "revision": "29d887c2aea3fb65dc6d",
+    "url": "/covid/static/js/runtime-main.b3dccd4d.js"
+  }
+]);
